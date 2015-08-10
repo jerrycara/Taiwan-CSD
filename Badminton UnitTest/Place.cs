6f65f69ae1a24c1fabdceb3f8f7a44b12d04319c
kodofish <kodofish@gmail.com>
2015-08-10 19:52:42 +0800

    Day 2 commit

---
+++ Badminton UnitTest/Place.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    public class IPlace
+    {
+    }
+
+    public class Place : IPlace
+    {
+        private string _address;
+        private string _endTime;
+        private int _price;
+        private string _startTime;
+
+        public Place(string address, string startTime, string endTime, int Price)
+        {
+            this._address = address;
+            this._startTime = startTime;
+            this._endTime = endTime;
+            this._price = Price;
+        }
+    }
+
+    public class Places
+    {
+        
+        public Places()
+        {
+            place = new List<Place>();    
+        }
+        public IList<Place> place { get; set; }
+    }
+}
---
+++ Badminton UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Manager_Add_PlaceInfo()
+        {
+            //Give
+            Place place = new Place("台北羽球館", "08:00", "10:00" , 100);
+            Places Places = new Places();
+            //When
+            Places.place.Add(place);
+            //Then
+            Assert.AreEqual(1, Places.place.Count);
+            Assert.AreEqual(place, Places.place[0]);
+        }
+    }
+}
