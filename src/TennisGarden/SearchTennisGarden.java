329f9df03eea7ec79077954f7d864a772d734019
elliotchiang <elliotchiang@gmail.com>
2015-08-10 19:46:21 +0800

    test

---
+++ src/TennisGarden/SearchTennisGarden.java
@@ -0,0 +1,5 @@
+package TennisGarden;
+
+public class SearchTennisGarden {
+
+}
---
+++ src/TennisGarden/SearchTests.java
@@ -0,0 +1,15 @@
+package TennisGarden;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class SearchTests {
+	@Test
+	public void given_SearchTennisGarden_when_Have_then_Return_TennisGarden(){
+		assertEquals("Taipei","Taipei");
+	}
+	
+}
